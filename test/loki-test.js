1898f1fd5e2c986a4f25134975695fa2d2c60306
Craig Condon <craig.condon@hellosign.com>
2015-06-19 17:56:17 -0700

    add cases

--- test/loki-test.js
+++ test/loki-test.js
@@ -3,17 +3,17 @@ var mesh   = require("mesh");
 var expect = require("expect.js");
 var _      = require("highland");
 var loki   = require("lokijs");
-var createTestCases      = require("mesh-store-test-cases");
+var createTestCases      = require("mesh/test-cases/database");
 
 describe(__filename + "#", function() {
 
-
-  // var cases = createTestCases(lokidb);
-  // for (var name in cases) it(name, cases[name]);
-
-  it("can be created", function() {
-    lokidb();
+  var cases = createTestCases(function() {
+    return lokidb();
   });
+
+  for (var name in cases) {
+    it(name, cases[name]);
+  }
 
   it("can pass in a loki instance", function() {
     var target = new loki();
@@ -21,190 +21,13 @@ describe(__filename + "#", function() {
     expect(db.target).to.be(target);
   });
 
-  it("emits an error if collection is missing", function(next) {
-    var db = mesh.clean(lokidb());
-    db("insert", { data: { name: "abba" }}).on("error", function() {
-      next();
-    });
-  });
-
-  it("can insert an item", function(next) {
-    var db = mesh.clean(mesh.child(lokidb(), { collection: "people" }));
-    db("insert", { data: { name: "abba" }}).on("data", function(item) {
-      expect(item.meta).not.to.be(void 0);
-      next();
-    });
-  });
-
   it("can insert an item with a loki id", function(next) {
-    var db = mesh.clean(mesh.child(lokidb(), { collection: "people" }));
-    db("insert", { data: { name: "abba", $loki: 1 }}).on("data", function(item) {
+
+    var db = lokidb();
+
+    db({ name: "insert", collection: "people", data: { name: "abba", $loki: 1 }}).on("data", function(item) {
       expect(item.meta).not.to.be(void 0);
       next();
-    });
-  });
-
-  it("can insert multiple items", function(next) {
-    var db = mesh.clean(mesh.child(lokidb(), { collection: "people" }));
-    db("insert", { data: [{ name: "abba" }, { name: "baab"}]}).pipe(_.pipeline(_.collect)).on("data", function(items) {
-      expect(items[0].name).to.be("abba");
-      expect(items[1].name).to.be("baab");
-      next();
-    });
-  });
-
-  it("can update an item", function(next) {
-    var db = mesh.clean(mesh.child(lokidb(), { collection: "people" }));
-
-    db("insert", {
-      data: { name: "abba" }
-    }).on("data", function(item) {
-
-      db("update", {
-        query: { name: "abba" },
-        data: { age: 17 }
-      }).on("data", function(item) {
-
-        expect(item.name).to.be("abba");
-        expect(item.age).to.be(17);
-        next();
-      });
-    });
-  });
-
-  it("can update multiple items", function(next) {
-    var db = mesh.clean(mesh.child(lokidb(), { collection: "people" }));
-    db("insert", {
-      data: [
-        { name: "abba" },
-        { name: "abba" }
-      ]
-    }).pipe(_.pipeline(_.collect)).on("data", function(items) {
-
-      expect(items.length).to.be(2);
-
-      db("update", {
-        multi: true,
-        query: { name: "abba" },
-        data: { age: 17 }
-      }).pipe(_.pipeline(_.collect)).on("data", function(items) {
-
-        expect(items.length).to.be(2);
-        expect(items[0].name).to.be("abba");
-        expect(items[0].age).to.be(17);
-        expect(items[1].name).to.be("abba");
-        expect(items[1].age).to.be(17);
-        next();
-      });
-    });
-  });
-
-  it("can load one item", function(next) {
-    var db = mesh.clean(mesh.child(lokidb(), { collection: "people" }));
-    db("insert", {
-      data: [
-        { name: "abba" },
-        { name: "abba" }
-      ]
-    }).pipe(_.pipeline(_.collect)).on("data", function() {
-      db("load", {
-        query: { name: "abba" }
-      }).on("data", function(person) {
-        expect(person.name).to.be("abba");
-        next();
-      });
-    });
-  });
-
-  it("can load multiple items", function(next) {
-    var db = mesh.clean(mesh.child(lokidb(), { collection: "people" }));
-    db("insert", {
-      data: [
-        { name: "abba" },
-        { name: "abba" }
-      ]
-    }).pipe(_.pipeline(_.collect)).on("data", function() {
-      db("load", {
-        multi: true,
-        query: { name: "abba" }
-      }).pipe(_.pipeline(_.collect)).on("data", function(items) {
-        expect(items.length).to.be(2);
-        next();
-      });
-    });
-  });
-
-  it("can remove one item", function(next) {
-    var db = mesh.clean(mesh.child(lokidb(), { collection: "people" }));
-
-    db("insert", {
-      data: [
-        { name: "abba" },
-        { name: "abba" }
-      ]
-    }).pipe(_.pipeline(_.collect)).on("data", function() {
-      db("remove", {
-        query: { name: "abba" }
-      }).on("end", function() {
-
-        db("load", {
-          query: { name: "abba" }
-        }).pipe(_.pipeline(_.collect)).on("data", function(items) {
-          expect(items.length).to.be(1);
-          next();
-        });
-      });
-    });
-  });
-
-  it("can remove multiple items", function(next) {
-    var db = mesh.clean(mesh.child(lokidb(), { collection: "people" }));
-
-    db("insert", {
-      data: [
-        { name: "abba" },
-        { name: "abba" }
-      ]
-    }).pipe(_.pipeline(_.collect)).on("data", function() {
-      db("remove", {
-        multi: true,
-        query: { name: "abba" }
-      }).on("end", function() {
-
-        db("load", {
-          query: { name: "abba" }
-        }).pipe(_.pipeline(_.collect)).on("data", function(items) {
-          expect(items.length).to.be(0);
-          next();
-        });
-      });
-    });
-  });
-
-  it("can upsert and insert an item", function(next) {
-    var db = mesh.clean(mesh.child(lokidb(), { collection: "people" }));
-    db("upsert", {
-      query: { name: "abba" },
-      data: { name: "abba" }
-    }).on("data", function(item) {
-      expect(item.meta).not.to.be(void 0);
-      next();
-    });
-  });
-
-  it("can upsert and update an item", function(next) {
-    var db = mesh.clean(mesh.child(lokidb(), { collection: "people" }));
-    db("upsert", {
-      query: { name: "abba" },
-      data: { name: "abba" }
-    }).on("data", function(insertedItem) {
-      db("upsert", {
-        query: { name: "abba" },
-        data: { name: "abba" }
-      }).on("data", function(updatedItem) {
-        expect(insertedItem.$loki).to.be(updatedItem.$loki);
-        next();
-      });
     });
   });
 });
